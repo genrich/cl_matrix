4c63f40a07246226b6188b9efb369ba7ec77fe91
genrich <9enrich@gmail.com>
2013-01-17 11:37:39 +0200

    formatting

--- backprop.m
+++ backprop.m
@@ -1,22 +1,22 @@
-function model = backprop(hiddenUnits, inputs, targets, trainIterations)
-    inputUnits  = size(inputs,  1);
-    outputUnits = size(targets, 1);
-    samples     = size(inputs,  2);
+function model = backprop (hiddenUnits, inputs, targets, trainIterations)
+    inputUnits  = size (inputs,  1);
+    outputUnits = size (targets, 1);
+    samples     = size (inputs,  2);
 
     t  = targets;
     m  = samples;
-    w1 = initWeights(hiddenUnits, inputUnits);
-    w2 = initWeights(outputUnits, hiddenUnits);
+    w1 = initWeights (hiddenUnits, inputUnits);
+    w2 = initWeights (outputUnits, hiddenUnits);
     y0 = inputs;
 
     for i = 1:trainIterations
-        y1 = sigmoid(w1 * y0);
-        y2 = sigmoid(w2 * y1);
+        y1 = sigmoid (w1 * y0);
+        y2 = sigmoid (w2 * y1);
 
-        % E = 1 / (2 * m) * sum(sum((t - y2) .^ 2));
+        % E = 1 / (2 * m) * sum (sum ((t - y2) .^ 2));
 
-        d_E_z2 = sigmoid(y2) .* (1 - sigmoid(y2)) .* (y2 - t);
-        d_E_z1 = sigmoid(y1) .* (1 - sigmoid(y1)) .* (w2' * d_E_z2);
+        d_E_z2 = sigmoid (y2) .* (1 - sigmoid (y2)) .* (y2 - t);
+        d_E_z1 = sigmoid (y1) .* (1 - sigmoid (y1)) .* (w2' * d_E_z2);
 
         d_E_w2 = d_E_z2 * y1' / m;
         d_E_w1 = d_E_z1 * y0' / m;
@@ -28,10 +28,10 @@ function model = backprop(hiddenUnits, inputs, targets, trainIterations)
     model.outputWeights = w2;
 end
 
-function w = initWeights(in, out)
+function w = initWeights (in, out)
     w = reshape (0.1 * sin (1:in * out), in, out);
 end
 
-function y = sigmoid(x)
-    y = 1 ./ (1 + exp(-x));
+function y = sigmoid (x)
+    y = 1 ./ (1 + exp (-x));
 end
--- backprop_cl.m
+++ backprop_cl.m
@@ -1,23 +1,23 @@
-function model = backprop_cl(hiddenUnits, inputs, targets, trainIterations)
-    inputUnits  = size(inputs,  1);
-    outputUnits = size(targets, 1);
-    samples     = size(inputs,  2);
+function model = backprop_cl (hiddenUnits, inputs, targets, trainIterations)
+    inputUnits  = size (inputs,  1);
+    outputUnits = size (targets, 1);
+    samples     = size (inputs,  2);
 
     t  = targets;
     m  = samples;
-    w1 = cl_matrix(initWeights(hiddenUnits, inputUnits));
-    w2 = cl_matrix(initWeights(outputUnits, hiddenUnits));
-    y0 = cl_matrix(inputs);
+    w1 = cl_matrix (initWeights (hiddenUnits, inputUnits));
+    w2 = cl_matrix (initWeights (outputUnits, hiddenUnits));
+    y0 = cl_matrix (inputs);
 
     for i = 1:trainIterations
         y1 = w1 * y0;
-        % y1 = sigmoid(w1 * y0);
-        % y2 = sigmoid(w2 * y1);
+        % y1 = sigmoid (w1 * y0);
+        % y2 = sigmoid (w2 * y1);
 
-        % 1 / (2 * m) * sum(sum((t - y2) .^ 2));
+        % 1 / (2 * m) * sum (sum ( (t - y2) .^ 2));
 
-        % d_E_z2 = sigmoid(y2) .* (1 - sigmoid(y2)) .* (y2 - t);
-    %     d_E_z1 = sigmoid(y1) .* (1 - sigmoid(y1)) .* (w2' * d_E_z2);
+        % d_E_z2 = sigmoid (y2) .* (1 - sigmoid (y2)) .* (y2 - t);
+    %     d_E_z1 = sigmoid (y1) .* (1 - sigmoid (y1)) .* (w2' * d_E_z2);
 
     %     d_E_w2 = d_E_z2 * y1' / m;
     %     d_E_w1 = d_E_z1 * y0' / m;
@@ -25,10 +25,10 @@ function model = backprop_cl(hiddenUnits, inputs, targets, trainIterations)
     %     w2 -= 0.5 * d_E_w2;
     %     w1 -= 0.5 * d_E_w1;
     end
-    model.hiddenWeights = double(w1);
-    model.outputWeights = double(w2);
+    model.hiddenWeights = double (w1);
+    model.outputWeights = double (w2);
 end
 
-function w = initWeights(in, out)
+function w = initWeights (in, out)
     w = reshape (0.1 * sin (1:in * out), in, out);
 end
